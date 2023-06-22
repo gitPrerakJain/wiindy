2a268ec4b41e9b4a54eecce3f1ef96816aea5af3
Prerak Jain <125865568+gitPrerakJain@users.noreply.github.com>
2023-06-22 16:11:04 +0530

    Added SVGs for weather condtions
    imported SVGs from Meteocons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="App">
+      React
+      <img src="../openweathermap/01d.svg" width={"100px"}></img>
+    </div>
+  );
 }
 
 export default App;
