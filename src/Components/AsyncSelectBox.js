e2fccd8d00b871980dba07e478457826dde8b279
Prerak Jain <125865568+gitPrerakJain@users.noreply.github.com>
2023-10-03 12:22:54 +0530

    Update AsyncSelectBox.js

--- src/Components/AsyncSelectBox.js
+++ src/Components/AsyncSelectBox.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 const BASE_API_URL =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&";
-const LOCATION_COORDS_BASE_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=`;
+const LOCATION_COORDS_BASE_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=`;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const AsyncSelectBox = ({ childToParent }) => {
